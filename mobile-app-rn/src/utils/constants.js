49ad75d78945a5ba31f2f252a53081f25ae204c3
Theekshana Alahakoon <theek237@gmail.com>
2026-06-10 18:50:28 +0530

    feat: add AppConstants utility with API endpoint configuration

--- mobile-app-rn/src/utils/constants.js
+++ mobile-app-rn/src/utils/constants.js
@@ -1,5 +1,6 @@
 export const AppConstants = {
   // Use 10.0.2.2 for Android Emulator connecting to localhost
   // Since we are running with Expo, we can also use our machine's local IP or localhost.
-  apiUrl: 'http://10.0.2.2:5005/api/v1',
+  // apiUrl: 'http://10.0.2.2:5005/api/v1',
+  apiUrl: 'https://upset-steaks-hide.loca.lt/api/v1',
 };
